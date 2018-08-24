0a5bd64380d302a3ab127ce285a6747c198f0e2a
Jaimeb2006 <jaimeb_2006@hotmail.com>
2018-08-24 09:45:25 -0500

    correcion componeste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './componentes/home-page/home-page.component';
+import {LoginPageComponent} from './componentes/login-page/login-page.component';
+import {RegisterPageComponent} from './componentes/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: '', component: HomePageComponent},
+   {path: 'login', component: LoginPageComponent},
+   {path: 'registro', component: RegisterPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
